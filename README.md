403be8e7db275bd54219b6d6c8181b04d66cc229
munavarm74 <51493786+munavarm74@users.noreply.github.com>
2023-09-21 18:44:34 +0100

    v0.1

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 Commands to run :
 
+Commandas to Setup :
+=====================
 1. composer update 
+2. php artisan key:generate
+
+Commands to Run :
+=====================
 2. php artisan serve 
 3. For Vue.js 
     1. npm run build
     2. npm run dev
-    
--- resources/js/Components/Footer.vue
+++ resources/js/Components/Footer.vue
@@ -5,6 +5,6 @@
         <div class="pull-right hidden-xs">
           <b>Version</b> 1.0
         </div>
-        <strong>Copyright &copy; 2020 - {{ new Date().getFullYear() }} <a target="_blank" href="http://saithy.uk/">Saity Uk</a>.</strong> All rights reserved.
+        <strong>Copyright &copy; {{ new Date().getFullYear() }} <a target="_blank" href="http://greenvalleyinfra.com">Green Valley Projects</a>.</strong> All rights reserved.
     </footer>
 </template>
